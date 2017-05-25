0d97b871122ff4c76888a08281a0b1eb2f7614c5
auromita93 <nagcha@uw.edu>
2017-05-25 09:02:59 -0400

    local storage

---
+++ storage.js
@@ -0,0 +1,110 @@
+'use strict';
+var information = [];
+//stores all people and their info
+
+$("#addNew").click(function(){
+    formPage();
+});
+
+$("#cancel").click(function(){
+    dataPage();
+})
+
+$("#submit").click(function(){
+    event.preventDefault();
+    information.push({
+        name: $("#name").val(),
+        age: $("#age").val()
+});
+dataPage();
+displayData();
+saveData();
+$("input").val();
+});
+
+//table data showcase
+function displayData(){
+ $("#dataPage").empty();
+ var tr, td, button;
+    for(var i = 0; i < information.length; i++){
+    tr = $("<tr>");
+    
+    td = $("<td>");
+    td.text(information[i].name);
+    tr.append(td);
+    
+    td = $("<td>");
+    td.text(information[i].age);
+    
+    //adding edit/delete button in tbody
+    td =$("<td>");
+    button = $("<button type='button' class='edit'");
+    button.text("Edit");
+    td.append(button);
+    button = $("<button type='button' class='delete'");
+    button.text("Delete");
+    td.append(button);
+    tr.append(td);
+    
+    $("#dataPage").append(tr);   
+}
+}
+
+//delete button
+$(".delete").click(function() {
+    $(this).parents("tr").remove();
+    var deleteButton = $(this).parents().siblings(":first").text();
+    information = information.filter(function(inf) {
+        return inf.name !== deleteButton;
+    });
+    saveData();
+});
+
+//edit button
+$(".edit").click(function() {
+    var editButton = $(this).parents().siblings(":first").text();
+    var editInfo = $.grep(information, function(inf) {
+        return inf.name === editButton;
+    }) [0];
+    formPage();
+    $("#name").val(editInfo.name);
+    $("#age").val(editInfo.age);  
+    information = information.filter(function(inf) {
+        return inf.title !==editButton;
+    });
+
+});
+
+
+//on click show form page
+function formPage(){
+   $("#dataPage").hide();
+   $("#enterForm").show();
+}
+
+//on click show data page
+function dataPage (){
+    $("#dataPage").show();
+    $("#enterForm").hide();
+}
+
+
+//local storage
+function storeInfo(){
+    var infoStorage = localStorage["information"];
+    if(infoStorage){
+        information = JSON.parse(infoStorage);
+    }
+    else{
+        informaton = [];
+    }
+    displayData();
+    
+}
+
+function saveData(){
+    localStorage["information"] = JSON.stringify(information);
+}
+
+
+
